48dcd1ec8a6ce3906db87f757ee197f87d89cb74
parveenchauhan <pkumar@lewanddowski.com>
2023-06-05 16:20:04 +0530

    cleanup the code

--- models/index.js
+++ models/index.js
@@ -6,27 +6,11 @@ const mongoose = require("mongoose");
 const { MongoClient } = require("mongodb");
 mongoose.set("strictQuery", false);
 
-const dbObj = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
-  dialect: config.dialect,
-  operatorsAliases: false,
-  port: process.env.DB_PORT,
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
-});
-
 const mongoDbClient = new MongoClient(config.MONGODBHOST);
 mongoose.connect(config.MONGODBHOST);
 
 const db = {};
 
-db.Sequelize = Sequelize;
-db.dbObj = dbObj;
-db.Op = Op;
 db.mongoDbClient = mongoDbClient;
 
 module.exports = db;
